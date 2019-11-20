a3090c0a3f126d7aa4074e785c1c0a59dfcfaf5e
e2167369 <e2167369@d.project.ch-geni-net.instageni.nps.edu>
2019-11-20 14:37:57 -0800

    bugfix

--- d/d_discovery.py
+++ d/d_discovery.py
@@ -13,7 +13,7 @@ def UDPServer(localIP, localPort):
         UDPServerSocket.sendto(message, address)
 
 if __name__ == "__main__":
-    interfaceIPs = {"r1": "10.10.4.2", "r2": "10.10.5.2", "r3": "10.10.7.2"}
+    interfaceIPs = {"r1": "10.10.4.2", "r2": "10.10.5.2", "r3": "10.10.7.1"}
     threads = []
     for key in interfaceIPs.keys():
         t = threading.Thread(target=UDPServer, args=(interfaceIPs[key], 4444))
