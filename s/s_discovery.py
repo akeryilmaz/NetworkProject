1024b3a705d5810d6842d3814e6513d87a244c63
e2167369 <e2167369@s.project.ch-geni-net.instageni.nps.edu>
2019-11-20 14:38:30 -0800

    bugfix

--- s/s_discovery.py
+++ s/s_discovery.py
@@ -4,6 +4,7 @@ import threading
 
 def UDPServer(localIP, localPort):
     # Create UDP Server socket and bind local IP & port to it.
+    print("Trying to create socket with {} and {}".format(localIP, localPort))
     UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDPServerSocket.bind((localIP, localPort))
     while True:
@@ -13,7 +14,7 @@ def UDPServer(localIP, localPort):
         UDPServerSocket.sendto(message, address)
 
 if __name__ == "__main__":
-    interfaceIPs = {"r1": "10.10.1.2", "r2": "10.10.2.2", "r3": "10.10.3.2"}
+    interfaceIPs = {"r1": "10.10.1.1", "r2": "10.10.2.2", "r3": "10.10.3.1"}
     threads = []
     for key in interfaceIPs.keys():
         t = threading.Thread(target=UDPServer, args=(interfaceIPs[key], 4444))
